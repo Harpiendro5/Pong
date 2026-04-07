2ab789bc9980bf668479b401e29351bf52af077e
Harpiendro <wilsonharper07@gmail.com>
2026-04-06 20:42:09 -0400

    Add files via upload

---
+++ entities/ball.py
@@ -0,0 +1,17 @@
+import pygame
+import Pong.src.core.constants as constants
+import Pong.src.core.resources as resources
+
+
+class Ball:
+	def __init__(self, x, y, size):
+		pass
+
+	def update(self):
+		pass
+
+	def reset(self, x, y):
+		pass
+
+	def draw(self, screen):
+		pass
---
+++ entities/paddle.py
@@ -0,0 +1,14 @@
+import pygame
+import Pong.src.core.constants as constants
+import Pong.src.core.resources as resources
+
+
+class Paddle:
+	def __init__(self, x, y, width, height):
+		pass
+
+	def update(self):
+		pass
+
+	def draw(self, screen):
+		pass
