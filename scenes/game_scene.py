baaa1029cfafc0c5681f141c4dcf3c790b9dbb48
Harpiendro <wilsonharper07@gmail.com>
2026-04-06 20:41:23 -0400

    Add files via upload

---
+++ scenes/game_scene.py
@@ -0,0 +1,15 @@
+import pygame
+import Pong.src.core.constants as constants
+import Pong.src.core.resources as resources
+
+def init(): # Set global variables for both paddles, balls, and font
+	pass
+
+def handle_event(event): 
+	pass
+
+def update():
+	pass
+
+def draw(screen): 
+	screen.fill(constants.BLACK)
