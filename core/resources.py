d8dee3996bb8efe1326d71c32d16f20fed3ee8b5
Harpiendro <wilsonharper07@gmail.com>
2026-04-06 20:42:44 -0400

    Refactor font path initialization in resources.py

--- core/resources.py
+++ core/resources.py
@@ -5,7 +5,7 @@ import Pong.src.core.constants as constants
 pygame.font.init()
 
 try:
-	font_path = "C://Users//wilso//Desktop//All.py//Arcade Central//press_start_2p//PressStart2P-Regular.ttf"
+	font_path = ""
 	pygame.font.Font(font_path, 30)
 	scores_font = pygame.font.Font(font_path, 45)
 	pygame.font.Font(font_path, 90)
